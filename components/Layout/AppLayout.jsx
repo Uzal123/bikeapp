b752d3af44ff4213ca8a8452f9e641088b8c45ff
Ujjwal-1214 <ujwalpaswan1214@gmail.com>
2023-03-13 00:57:03 +0530

    tags

--- components/Layout/AppLayout.jsx
+++ components/Layout/AppLayout.jsx
@@ -3,7 +3,7 @@ import React, { Fragment } from "react";
 import Navbar from "../Navigation/Navbar";
 
 const AppLayout = ({ title, description, keywords, url, image, ...props }) => {
-  const defaultTitle = "MotoGhar - Buy and Rent Bikes and Cars";
+  const defaultTitle = "Buy and Rent Bikes and Cars";
   const defaultDescription =
     "Find the perfect bike or car for your needs at MotoGhar, the premier online marketplace for buying and renting vehicles.";
   const defaultKeywords = "MotoGhar, bikes, cars, rental, marketplace";
@@ -13,48 +13,50 @@ const AppLayout = ({ title, description, keywords, url, image, ...props }) => {
     <Fragment>
       <div className="main">
         <Head>
-          <title>{title ? "MotoGhar - " + title : defaultTitle}</title>
+          <title>{`MotoGhar -  ${title ? title : defaultTitle}`}</title>
           <meta
             name="description"
-            content={description ? description : defaultDescription}
+            content={`${description ? description : defaultDescription}`}
           />
           <meta
             name="keywords"
-            content={keywords ? keywords : defaultKeywords}
+            content={`${keywords ? keywords : defaultKeywords}`}
           />
           <link
             rel="icon"
             href="www.motoghar.com/RentCar.png"
             type="image/png"
           />
-          <link
-            rel="shortcut icon"
-            href="/favicon.ico"
-            type="image/x-icon"
-          />
-          <link rel="canonical" href={url ? url : defaultUrl} />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="canonical" href={`${url ? url : defaultUrl}`} />
           <meta
             property="og:title"
-            content={title ? "MotoGhar - " + title : defaultTitle}
+            content={`MotoGhar -  ${title ? title : defaultTitle}`}
           />
           <meta
             property="og:description"
-            content={description ? description : defaultDescription}
+            content={`${description ? description : defaultDescription}`}
           />
-          <meta property="og:url" content={url ? url : defaultUrl} />
-          <meta property="og:image" content={image ? image : defaultImage} />
+          <meta property="og:url" content={`${url ? url : defaultUrl}`} />
+          <meta
+            property="og:image"
+            content={`${image ? image : defaultImage}`}
+          />
           <meta property="og:type" content="website" />
           <meta
             name="twitter:title"
-            content={title ? "MotoGhar - " + title : defaultTitle}
+            content={`MotoGhar -  ${title ? title : defaultTitle}`}
           />
           <meta
             name="twitter:description"
-            content={description ? description : defaultDescription}
+            content={`${description ? description : defaultDescription}`}
           />
 
-          <meta name="twitter:url" content={url ? url : defaultUrl} />
-          <meta name="twitter:image" content={image ? image : defaultImage} />
+          <meta name="twitter:url" content={`${url ? url : defaultUrl}`} />
+          <meta
+            name="twitter:image"
+            content={`${image ? image : defaultImage}`}
+          />
           <meta name="twitter:card" content="summary_large_image" />
           <meta property="og:street-address" content="Kathmandu" />
           <meta property="og:locality" content="Kathmandu" />
